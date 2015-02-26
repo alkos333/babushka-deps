ccab74044cbf7bfce37cec9951270a057ae0a67c
alkos333 <me@alkos333.net>
2015-02-26 07:05:12 -0600

    git-prompt support for inclusion in bash prompt

---
+++ git-prompt.rb
@@ -0,0 +1,8 @@
+dep 'git-prompt' do
+	met? {
+		"~/.git-prompt.sh".p.exists?
+	}
+	meet {
+		shell "curl -LSso ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
+	}
+end

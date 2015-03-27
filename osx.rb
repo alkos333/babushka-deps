7aa571fd4786a6298535efde80cd8567aad00d01
alkos333 <me@alkos333.net>
2015-03-26 21:35:51 -0500

    VisualVM usually gets shipped with Java

--- osx.rb
+++ osx.rb
@@ -6,7 +6,7 @@ dep "XQuartz.app" do
 end
 
 dep "Inkscape.app" do
-  requires "XQuartz.app" 
+#  requires "XQuartz.app" 
   source "https://inkscape.org/en/gallery/item/3896/Inkscape-0.91-1-x11-10.7-x86_64.dmg"
 end
 
@@ -18,14 +18,9 @@ dep "Spectacle.app" do
   source "https://s3.amazonaws.com/spectacle/downloads/Spectacle+0.8.8.zip"
 end
 
-dep "VisualVM.app" do 
-  source "https://java.net/projects/visualvm/downloads/download/release138/VisualVM_138.dmg"
-end
-
 dep "osx-all-apps" do
   requires "Inkscape.app"
   requires "Spectacle.app"
-  requires "VisualVM.app"
 end
 
 dep "jetbrains-paid" do

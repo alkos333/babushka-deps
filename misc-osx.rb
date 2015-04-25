6e76e7643a67966464249d332f2170db195e6a0a
alkos333 <me@alkos333.net>
2015-04-25 15:26:36 -0500

    Remove IntelliJ

--- misc-osx.rb
+++ misc-osx.rb
@@ -10,10 +10,6 @@ dep "Inkscape.app" do
   source "https://inkscape.org/en/gallery/item/3896/Inkscape-0.91-1-x11-10.7-x86_64.dmg"
 end
 
-dep "IntelliJ.app" do
-  source "http://download.jetbrains.com/idea/ideaIU-14.0.3.dmg"
-end
-
 dep "Spectacle.app" do
   source "https://s3.amazonaws.com/spectacle/downloads/Spectacle+0.8.8.zip"
 end
@@ -26,8 +22,4 @@ dep "osx-all-apps" do
   requires "Inkscape.app"
   requires "Spectacle.app"
   requires "HipChat.app"
-end
-
-dep "jetbrains-paid" do
-  requires "IntelliJ.app" 
 end

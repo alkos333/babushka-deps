9292f5f0b9c284d756009ac49c7f726fc07a7ba2
alkos333 <me@alkos333.net>
2015-04-29 03:53:45 -0500

    Try howdoi command

--- misc-dev-tools.rb
+++ misc-dev-tools.rb
@@ -14,9 +14,14 @@ dep "thefuck.bin" do
   provides "thefuck"
 end
 
+dep "howdoi.pip" do
+  provides "howdoi"
+end
+
 dep "misc-dev-tools" do
   requires "wget.bin"
   requires "jq.bin"
   requires "tmux.bin"
   requires "thefuck.bin"
+  requires "howdoi.pip"
 end

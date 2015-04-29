b0497706534675ace7f8c91cb0bd02e044b863de
alkos333 <me@alkos333.net>
2015-04-29 10:01:57 -0500

    Add boot2docker

--- misc-dev-tools.rb
+++ misc-dev-tools.rb
@@ -10,18 +10,19 @@ dep "tmux.bin" do
   provides "tmux"
 end
 
-dep "thefuck.bin" do
-  provides "thefuck"
-end
-
 dep "howdoi.pip" do
   provides "howdoi"
 end
+
+dep "boot2docker.bin" do
+  provides "boot2docker"
+end
+
 
 dep "misc-dev-tools" do
   requires "wget.bin"
   requires "jq.bin"
   requires "tmux.bin"
-  requires "thefuck.bin"
   requires "howdoi.pip"
+  requires "boot2docker.bin"
 end
